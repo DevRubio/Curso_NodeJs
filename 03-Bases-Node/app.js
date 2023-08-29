7873b18c840dfc87a1e002666e959aa4ff6eebc8
oscar.franco <ofrubio0@gmail.com>
2023-08-29 18:32:32 -0500

    Recibir información desde línea de comando

--- 03-Bases-Node/app.js
+++ 03-Bases-Node/app.js
@@ -1,11 +1,21 @@
 
 const { CrearArchivo } = require('./helpers/multiplicar')
 
-const base = 3
+//const base = 3
 
 console.clear()
 
-CrearArchivo(20)
+//Se utiliza para ver los argumentos que vienen en la consola
+//console.log(process.argv)
+
+const [ , , arg3='base=5'] = process.argv
+
+const [, base=5] = arg3.split('=')
+
+console.log(base)
+
+
+CrearArchivo(base)
     .then(response => console.log(response))
     .catch(err => console.error(err))
 
