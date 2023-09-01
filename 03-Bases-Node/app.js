4d05b8f30feba3fd01526f6e3fd8da31904666dc
oscar.franco <ofrubio0@gmail.com>
2023-09-01 17:46:34 -0500

    yargs

--- 03-Bases-Node/app.js
+++ 03-Bases-Node/app.js
@@ -1,18 +1,21 @@
-
 const { CrearArchivo } = require('./helpers/multiplicar')
+const argv = require('yargs').argv
 
 //const base = 3
 
 console.clear()
 
+console.log(process.argv)
+console.log(argv)
+
+console.log('base: yargs',argv.base)
+
 //Se utiliza para ver los argumentos que vienen en la consola
 //console.log(process.argv)
 
-const [ , , arg3='base=5'] = process.argv
-
+/* const [ , , arg3='base=5'] = process.argv
 const [, base=5] = arg3.split('=')
 
-console.log(base)
 
 
 CrearArchivo(base)
@@ -20,5 +23,5 @@ CrearArchivo(base)
     .catch(err => console.error(err))
 
 
-
+ */
 
